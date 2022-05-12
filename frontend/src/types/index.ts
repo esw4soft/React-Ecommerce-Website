9bbeb33df57f90cf198eda606779213d7e147eca
easonw <es250528@gmail.com>
2022-05-12 18:15:43 +0800

    data ts

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,16 @@
+interface Productdet {
+  name: string
+  slug: string
+  category: string
+  image: string
+  price: number
+  countInStock: number
+  brand: string
+  rating: number
+  numReviews: number
+  description: string
+}
+
+export interface Product {
+  products: [Productdet, Productdet, Productdet, Productdet]
+}
